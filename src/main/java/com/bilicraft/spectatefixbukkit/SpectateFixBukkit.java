908c7cd31d250ca0c556116a9890b73f08d188ed
Ghost_chu <2908803755@qq.com>
2021-07-19 18:13:47 +0800

    Initial commit

---
+++ src/main/java/com/bilicraft/spectatefixbukkit/SpectateFixBukkit.java
@@ -0,0 +1,44 @@
+package com.bilicraft.spectatefixbukkit;
+
+import com.google.gson.Gson;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+
+public final class SpectateFixBukkit extends JavaPlugin {
+    private final Gson gson = new Gson();
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getMessenger().registerIncomingPluginChannel(this, "spectatefix:tp", (channel, receiver, message) -> {
+            MessagePacket messagePacket = gson.fromJson(new String(message), MessagePacket.class);
+            getLogger().info("Patching the spectate packet: "+messagePacket);
+            Player player = Bukkit.getPlayer(messagePacket.getPlayer());
+            if(player == null){
+                return;
+            }
+            if(player.getGameMode() != GameMode.SPECTATOR){
+                return;
+            }
+            Player toPlayer = Bukkit.getPlayer(messagePacket.getTarget());
+            if(toPlayer == null){
+                return;
+            }
+            PlayerTeleportEvent event = new PlayerTeleportEvent(player,player.getLocation(),toPlayer.getLocation());
+            Bukkit.getPluginManager().callEvent(event);
+            if(event.isCancelled()){
+                return;
+            }
+            player.teleport(toPlayer);
+        });
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
